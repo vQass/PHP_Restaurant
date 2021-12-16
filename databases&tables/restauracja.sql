93ed68c2d71d648833b848b3dae023e868004f14
Patryk Laptop <Patryk.Zub@interia.pl>
2021-12-16 15:51:03 +0100

    Baza danych z przykładami do testów przed obroną projektu

--- databases&tables/restauracja.sql
+++ databases&tables/restauracja.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 14 Gru 2021, 16:19
+-- Czas generowania: 16 Gru 2021, 15:50
 -- Wersja serwera: 10.4.13-MariaDB
 -- Wersja PHP: 7.4.8
 
@@ -61,17 +61,17 @@ CREATE TABLE `menu` (
 --
 
 INSERT INTO `menu` (`id`, `name`, `category`, `price`, `photo`, `description`) VALUES
-(1, 'Texas', 'Pizza', 30.99, 'texas.jpg', 'Składniki:\r\n-ser\r\n-cebula\r\n-boczek\r\n-kukurydza'),
-(2, 'margherita', 'Pizza', 25.58, 'mar.jpg', 'Wyśmienita pizza.\r\nPolecam!\r\nBardzo!'),
-(3, 'Piwo wolnościowe', 'Piwo', 7.99, 'piwo_wolnosciowe.png', 'Bardzo dobre'),
-(4, 'Polska wolność', 'Piwo', 7.99, 'polska_wolnosc.png', 'Bardzo dobre!'),
-(5, 'Paszport Mentzena', 'Piwo', 9.99, 'paszport_mentzena.png', 'Wyśmienite'),
-(6, 'Wiejska', 'Pizza', 27.10, 'wiejska.jpg', 'Pizza prosto ze wsi!'),
-(7, 'Diavolo', 'Pizza', 28.99, 'diavolo.jpg', 'Bardzo dobra w smaku!'),
-(8, 'Peperoni', 'Pizza', 21.79, 'peperoni.jpg', 'Klasyk!'),
-(9, 'Bezobjawowe', 'Piwo', 6.99, 'bezobjawowe.png', 'Piwo 0%'),
-(10, 'White IPA Matters', 'Piwo', 10.99, 'white_ipa_matters.png', 'Doskonałe w smaku'),
-(11, 'Czekoladowa rozkosz', 'Pizza', 32.00, 'czekoladowa.jpg', 'Rozpływa się w ustach');
+(1, 'Texas', 'Pizza', 30.99, 'texas.jpg', 'Składniki:\r\n- sos barbecue\r\n- ser cheddar\r\n- czerwona cebula\r\n- boczek\r\n- kurczak\r\n- kukurydza\r\n- oregano'),
+(2, 'Margherita', 'Pizza', 25.58, 'mar.jpg', 'Składniki:\r\n- sos pomidorowy\r\n- ser mozzarella\r\n- bazylia'),
+(3, 'Piwo wolnościowe', 'Piwo', 7.99, 'piwo_wolnosciowe.png', 'Piwo pszeniczne, goryczka niska\r\nALK. 4 % | BLG 12'),
+(4, 'Polska wolność', 'Piwo', 7.99, 'polska_wolnosc.png', 'Piwo jasne w stylu Imperial Witbier, goryczka niska\r\nALK. 7 % | BLG 18'),
+(5, 'Paszport Mentzena', 'Piwo', 9.99, 'paszport_mentzena.png', 'Piwo jasne w stylu IPA, goryczka średnia\r\nALK. 6 % | BLG 15'),
+(6, 'Wiejska', 'Pizza', 27.10, 'wiejska.jpg', 'Składniki:\r\n- sos pomidorowy\r\n- ser cheddar\r\n- szynka\r\n- baleron\r\n- boczek\r\n- kiełbasa\r\n- cebula\r\n- pieczarki'),
+(7, 'Diavolo', 'Pizza', 28.99, 'diavolo.jpg', 'Składniki:\r\n- sos pomidorowy\r\n- pikantny ser pecorino\r\n- papryczka chilli\r\n- oliwki\r\n- salami'),
+(8, 'Peperoni', 'Pizza', 21.79, 'peperoni.jpg', 'Składniki:\r\n- sos pomidorowy\r\n- ser mozzarella\r\n- pepperoni\r\n- bazylia'),
+(9, 'Bezobjawowe', 'Piwo', 6.99, 'bezobjawowe.png', 'Piwo bezalkoholowe w stylu IPA, goryczka średnia\r\nALK. <0,5 % | BLG 7,8'),
+(10, 'White IPA Matters', 'Piwo', 10.99, 'white_ipa_matters.png', 'Piwo jasne w stylu White IPA, goryczka średnia\r\nALK. 6 % | BLG 15'),
+(11, 'Czekoladowa rozkosz', 'Pizza', 32.00, 'czekoladowa.jpg', 'Składniki:\r\n- czekolada\r\n- cukierki M&M\'s\r\n- batony Kit Kat oraz Milky Way\r\n- maliny');
 
 -- --------------------------------------------------------
 
@@ -97,7 +97,16 @@ INSERT INTO `orders` (`idOrders`, `idUser`, `idProduct`, `number`) VALUES
 (1, 16, 4, 1),
 (2, 16, 2, 1),
 (2, 16, 6, 1),
-(3, 16, 2, 1);
+(3, 16, 2, 1),
+(4, 16, 2, 1),
+(4, 16, 3, 2),
+(4, 16, 5, 1),
+(5, 16, 1, 4),
+(5, 16, 10, 2),
+(6, 17, 2, 2),
+(6, 17, 4, 2),
+(7, 22, 1, 1),
+(7, 22, 5, 2);
 
 -- --------------------------------------------------------
 
@@ -112,7 +121,7 @@ CREATE TABLE `ordersdetails` (
   `address` varchar(64) CHARACTER SET utf8 NOT NULL,
   `phone` varchar(9) CHARACTER SET utf8 NOT NULL,
   `discountCode` varchar(16) NOT NULL DEFAULT 'Brak',
-  `status` enum('W trakcie realizacji','zrealizowano','anulowano','') CHARACTER SET utf8 NOT NULL
+  `status` enum('W trakcie realizacji','Zrealizowano','Anulowano','') CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -120,9 +129,13 @@ CREATE TABLE `ordersdetails` (
 --
 
 INSERT INTO `ordersdetails` (`idOrders`, `name`, `city`, `address`, `phone`, `discountCode`, `status`) VALUES
-(1, 'Jan', 'Gliwice', 'Polna 2', '987654321', 'inflacja10', 'W trakcie realizacji'),
+(1, 'Jan', 'Gliwice', 'Polna 2', '987654321', 'inflacja10', 'Zrealizowano'),
 (2, 'synJana', 'Katowice', 'Lesna 2', '123456789', 'naKosztFirmy', 'W trakcie realizacji'),
-(3, 'Jan', 'Gliwice', 'Polna 2', '987654321', 'Brak', 'W trakcie realizacji');
+(3, 'Jan', 'Gliwice', 'Polna 2', '987654321', 'Brak', 'W trakcie realizacji'),
+(4, 'Patryk', 'Zabrze', 'Dworcowa 12/4', '098765432', 'inflacja10', 'W trakcie realizacji'),
+(5, 'Piotr', 'Zabrze', 'Dworcowa 12/4', '098765432', 'inflacja10', 'W trakcie realizacji'),
+(6, 'Piotr', 'Katowice', 'Polna 3', '987654321', 'Brak', 'W trakcie realizacji'),
+(7, 'Patryk', 'Gliwice', 'Polna3', '987654321', 'Brak', 'W trakcie realizacji');
 
 -- --------------------------------------------------------
 
@@ -150,7 +163,10 @@ INSERT INTO `users` (`id`, `name`, `permission`, `city`, `address`, `phone`, `is
 (16, 'Jan', 'user', 'Katowice', 'Mokra 5', '156485555', 1, '7c4a8d09ca3762af61e59520943dc26494f8941b', 'J@gmail.com'),
 (17, 'admin', 'admin', NULL, NULL, NULL, 1, '9d4e1e23bd5b727046a9e3b4b7db57bd8d6ee684', 'user@test.pl'),
 (18, 'Jakub', 'employee', 'Gliwice', 'tak 5', '168455555', 1, '9d4e1e23bd5b727046a9e3b4b7db57bd8d6ee684', 'xd@op.pl'),
-(19, NULL, 'user', NULL, NULL, NULL, 1, '22e60f213a388c2cc2872dcb2c9fedee66165bc8', 'Patryk@test.pl');
+(19, NULL, 'user', NULL, NULL, NULL, 1, '22e60f213a388c2cc2872dcb2c9fedee66165bc8', 'Patryk@test.pl'),
+(20, NULL, 'user', NULL, NULL, NULL, 1, '9d4e1e23bd5b727046a9e3b4b7db57bd8d6ee684', 'test@test.pl'),
+(21, NULL, 'employee', NULL, NULL, NULL, 1, '9d4e1e23bd5b727046a9e3b4b7db57bd8d6ee684', 'user2@test.pl'),
+(22, 'Patryk', 'user', 'Gliwice', 'Polna3', '987654321', 1, '9d4e1e23bd5b727046a9e3b4b7db57bd8d6ee684', 'user3@test.pl');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -200,7 +216,7 @@ ALTER TABLE `menu`
 -- AUTO_INCREMENT dla tabeli `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
